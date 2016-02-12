d7665b2d5e87a3c3f5e44ce7022cfdf2a29e556b
James Conroy-Finn <james@logi.cl>
2016-02-12 11:21:05 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 8; i++ {
+		fmt.Printf("\033[1m 4%d \033[0m", i)
+	}
+	fmt.Println()
+
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Printf("\033[3%d;4%dm 3%d \033[0m", i, j, i)
+		}
+		fmt.Println()
+	}
+}
